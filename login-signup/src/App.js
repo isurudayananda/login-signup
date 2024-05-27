b7926a8e9231898bde970b775cf25e4059c0e32f
isurudayananda <129027427+isurudayananda@users.noreply.github.com>
2024-05-27 22:07:43 +0530

    mount components in app js

--- login-signup/src/App.js
+++ login-signup/src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginSignup from './Components/Login-signup/Login-signup';
 
 function App() {
   return (
-    
+
+    <LoginSignup/>
+
   
   );
 }
