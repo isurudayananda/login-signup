df07a3dd39771579af34c6b39a0c4773a9ce6cf1
isurudayananda <129027427+isurudayananda@users.noreply.github.com>
2024-06-01 20:55:37 +0530

    import images to loginsignup jsx

--- login-signup/src/Components/Loginsignup/Loginsignup.jsx
+++ login-signup/src/Components/Loginsignup/Loginsignup.jsx
@@ -1,5 +1,8 @@
 import './Loginsignup.css'
 import React from 'react'
+import user_icon from '../Assests/person.png'
+import email_icon from '../Assests/email.png'
+import password_icon from '../Assests/password.png'
 
 const Loginsignup = () => {
   return (
