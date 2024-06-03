3c6df3e4a881734b8c45fb562bbacf1289b17a8a
isurudayananda <129027427+isurudayananda@users.noreply.github.com>
2024-06-03 15:53:06 +0530

    import usestate for js

--- login-signup/src/Components/Loginsignup/Loginsignup.jsx
+++ login-signup/src/Components/Loginsignup/Loginsignup.jsx
@@ -1,8 +1,13 @@
+import React, { useState } from 'react'
 import './Loginsignup.css'
-import React from 'react'
 import user_icon from '../Assests/person.png'
 import email_icon from '../Assests/email.png'
 import password_icon from '../Assests/password.png'
+
+const LoginSignup = () => {
+
+  const [action,setAction] = useState("Sign Up");
+
 
 const Loginsignup = () => {
   return (
